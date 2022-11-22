2f2e4994f0c22a7524c76e5fea8ee8c4243add22
ariannadepasquale <depasqualearianna@gmail.com>
2022-11-22 08:48:39 +0100

    Milestone 3

--- src/org/generation/italy/Biglietteria.java
+++ src/org/generation/italy/Biglietteria.java
@@ -6,23 +6,39 @@ public class Biglietteria {
     public static void main(String[] args) {
         
     	Scanner scanner = new Scanner(System.in);
-        int age; int km;
-        
-        System.out.println("Inserire il numero di km da percorrere:");
-        km = scanner .nextInt();
-        
-        System.out.println("Inserisci la tua età:");
-        age = scanner .nextInt();
+        boolean flessibile;
+        String sceltaFlex = "";
+            
         
         try {
-            Biglietto ticket = new Biglietto(km, age);
-            System.out.println(ticket);
-            //System.out.println("Il prezzo del tuo biglietto è: "+ ticket.calcolaPrezzo());
+        	 
+        	 
+             System.out.println("Inserire il numero di km da percorrere:");
+             int km = scanner .nextInt();
+             
+             System.out.println("Inserisci la tua età:");
+             int age = scanner .nextInt();
+             scanner.nextLine();
+             
+             System.out.println("Vuoi usufruire del metodo flessibile?: (si/no)");
+             sceltaFlex = scanner.nextLine();
+             
+             if(sceltaFlex.equalsIgnoreCase("si")) {
+             	flessibile = true;
+             } else {
+             	flessibile = false;
+             }
+             
+             Biglietto ticket = new Biglietto(km, age, flessibile);
+             System.out.println(ticket);
+             //System.out.println("Il prezzo del tuo biglietto è: "+ ticket.calcolaPrezzo());
+           
         
             
         } catch (Exception e) {
         	System.out.println("Inserisci età valida / Inserisci km validi");
         }
+        scanner.close();
     }
 
 }
--- src/org/generation/italy/Biglietto.java
+++ src/org/generation/italy/Biglietto.java
@@ -1,26 +1,34 @@
 package org.generation.italy;
-
 import java.math.BigDecimal;
+import java.time.LocalDate;
 
 public class Biglietto {
 	
 	
 	//costanti
-	static final BigDecimal COSTO_KM = new BigDecimal("0.21");
-	static final BigDecimal SCONTO_OVER = new BigDecimal("0.6");
-	static final BigDecimal SCONTO_UNDER = new BigDecimal("0.8");
+	static final BigDecimal COSTO_KM = new BigDecimal(".21");
+	static final BigDecimal SCONTO_OVER = new BigDecimal("0.4");
+	static final BigDecimal SCONTO_UNDER = new BigDecimal("0.2");
+	static final int NORMAL = 30;
+	static final int FLESSIBILE = 90;
 	
 	
 	
 	//attributi
 	private int km;
 	private int age;
+	private LocalDate date;
+	private boolean flessibile;
+	
 	
 	//costruttore
-	public Biglietto(int km,int age) throws Exception{
+	public Biglietto(int km,int age, boolean flessibile) throws Exception{
 		this.km = km;
 		this.age = age;
+		this.date = LocalDate.now();
+		this.flessibile = flessibile;
 		
+			
 		
 		if (!isValidkm() || !isValidAge()) {	
 			throw new Exception("Inserisci un valore valido");
@@ -56,14 +64,26 @@ public class Biglietto {
 	}
 	
 	public BigDecimal calcolaPrezzo() {
+		if (flessibile) {
 		return calcolaSconto().multiply(BigDecimal.valueOf(km).multiply(BigDecimal.valueOf(1.10)));
-		
+		}
+		return calcolaSconto().multiply(BigDecimal.valueOf(km));
+	}
+	
+	
+	public LocalDate calcolaDataScadenza() {
+		if (flessibile) {
+			return date.plusDays(FLESSIBILE);
+			
+		}else {
+			return date.plusDays(NORMAL);
+		}
 	}
 	
 	@Override
 	public String toString() {	
 		return "Il numero di km che vuoi percorrere: " + km + "\nLa tua età: " + age + "\nIl prezzo del tuo biglietto: " 
-	+ calcolaPrezzo() + "euro";
+	+ calcolaPrezzo() + " euro" + "\nData di scadenza del biglietto: " + calcolaDataScadenza();
 	}
 	
 	
